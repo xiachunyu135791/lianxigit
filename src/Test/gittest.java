dccaeaf97de43b2a46b23945d34114be9562e049
xiachunyu135791 <2282262512@qq.com>
2018-07-22 18:51:03 +0800

    add and update class

---
+++ src/Test/gittest.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class gittest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            System.out.println("第一次用GIT");
+            System.out.println("第二次用GIT123");
+	}
+
+}
