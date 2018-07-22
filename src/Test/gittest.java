724919df7b7aa2ef4963847323272287dc20e0b9
xiachunyu135791 <2282262512@qq.com>
2018-07-22 18:58:42 +0800

    xiugaihoude

--- src/Test/gittest.java
+++ src/Test/gittest.java
@@ -7,8 +7,8 @@ public class gittest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-            System.out.println("第一次用GIT");
-            System.out.println("第二次用GIT123");
+            System.out.println("xiachunyuGIT");
+            System.out.println("xiachunyuGIT123");
 	}
 
 }
